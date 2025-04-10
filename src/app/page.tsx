5b7bbe0f824810f23cbaa5b67e21cbee27efd9df
Frizqi <hirotokamenrider047@gmail.com>
2025-04-10 16:20:58 +0700

    fix page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoList from '../app/components/todolist';
 
 function page() {
   return (
     <>
       <h1>Landing Page </h1>
+      <TodoList />
     </>
   );
 }
