c9024c78a33abf25c9005131b165fe15da8ade30
Frizqi <hirotokamenrider047@gmail.com>
2025-04-11 09:33:00 +0700

    isi konfig firebase.ts

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // isi konfigurasi sesuai dengan konfigurasi firebase kalian
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyDJu_Y4Ub2ew1AyhMXMv3UqQ2_NzqhKlWM',
+  authDomain: 'todolist-960c3.firebaseapp.com',
+  projectId: 'todolist-960c3',
+  storageBucket: 'todolist-960c3.firebasestorage.app',
+  messagingSenderId: '1:240080086367:web:f9212fc4b47de1156402ea',
+  appId: 'G-0JX3K2F1Q4',
 };
 
 // Inisialisasi Firebase
