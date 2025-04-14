85f460c33cb23e223a2ad4d9b74cbda71342c65c
Frizqi <hirotokamenrider047@gmail.com>
2025-04-14 16:33:58 +0700

    Implement code changes to enhance functionality and improve performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "Task98",
   description: "To-Do List Retro Windows 98 Style",
   icons: {
-    icon: "/public/favicon.ico", // Pastikan favicon.ico berada di folder /public
+    icon: "../app/favicon.ico", // Pastikan favicon.ico berada di folder /public
   },
 };
 
